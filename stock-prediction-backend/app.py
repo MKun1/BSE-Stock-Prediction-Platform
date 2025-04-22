00a6f1716a25199f1915d6db22c4d58b2ea59011
Kundai Mduduzi <kundaimduduzi2@gmail.com>
2025-04-22 17:03:09 +0530

    Update app.py

--- stock-prediction-backend/app.py
+++ stock-prediction-backend/app.py
@@ -233,6 +233,5 @@ def save_to_database(name, email, subject, message):
 
 
 import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
